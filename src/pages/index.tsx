83cd9db0c46d99b444c974a9edfc7d965a8b5f55
Josh Liley <=>
2019-10-27 01:15:16 +0100

    updated section logic and added custom fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,74 +2,81 @@ import React from "react"
 import { Link } from "gatsby"
 import SectionedView from "../components/Layouts/SectionedView"
 import SEO from "../components/seo"
+import { FlexDirection } from "../components/PageSection/sectionBlock"
+import SectionBlock from "../components/PageSection/sectionBlock"
 import PageSection from "../components/PageSection/pageSection"
+import '../../src/styles/global.scss'
 
 const NewDesign = () => (
   <SectionedView>
-    <SEO title="New design" />
+    <SectionBlock direction={FlexDirection.Vertical} background="transparent">
+      <SEO title="New design" />
 
-    {/* Section One: About */}
-    <PageSection size="fill" color="#ddd" background="#0066dd">
-      <h1>This is the about section</h1>
-      <p>this is some information about me, I guess this will do for now</p>
-    </PageSection>
+      {/* Section One: About */}
+      <PageSection size="fill" color="#ddd" background="#0066dd">
+        <h1>This is the about section</h1>
+        <p>this is some information about me, I guess this will do for now</p>
+      </PageSection>
 
-    {/* Section Two: Projects */}
-    <PageSection
-      size="fill"
-      color="#666"
-      background="url('https://images.pexels.com/photos/1629236/pexels-photo-1629236.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260')"
-    >
-      <h1>This is the about section</h1>
-      <div
-        className="pf-subsection"
-        style={{ display: "flex", justifyContent: "center" }}
+      {/* Section Two: Projects */}
+      <PageSection
+        size="fill"
+        color="#666"
+        background="url('https://images.pexels.com/photos/1629236/pexels-photo-1629236.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260')"
       >
-        <PageSection size="large" color="#ddd" background="#0066dd">
-          <h1>This is the about section</h1>
-          <p>this is some information about me, I guess this will do for now</p>
-        </PageSection>
-        <PageSection size="large" color="#ddd" background="#0066dd">
-          <h1>This is the about section</h1>
-          <p>this is some information about me, I guess this will do for now</p>
-        </PageSection>
-      </div>
-    </PageSection>
+        <h1>This is the about section</h1>
+        <SectionBlock direction={FlexDirection.Horizontal} background="#aaaa">
+          <PageSection size="large" color="#ddd" background="#0066dd">
+            <h1>This is the about section</h1>
+            <p>
+              this is some information about me, I guess this will do for now
+            </p>
+          </PageSection>
+          <PageSection size="large" color="#ddd" background="#0066dd">
+            <h1>This is the about section</h1>
+            <p>
+              this is some information about me, I guess this will do for now
+            </p>
+          </PageSection>
+        </SectionBlock>
+      </PageSection>
 
-    {/* Section Three: Work */}
-    <PageSection size="small" color="#ddd" background="#12a5aa">
-      <h1>This is the about section</h1>
-      <p>this is some information about me, I guess this will do for now</p>
-    </PageSection>
+      {/* Section Three: Work */}
+      <PageSection size="small" color="#ddd" background="#12a5aa">
+        <h1>This is the about section</h1>
+        <p>this is some information about me, I guess this will do for now</p>
+      </PageSection>
 
-    {/* Section Four: Skills */}
-    <PageSection size="large" color="#ddd" background="#f555ff">
-      <h1>This is the about section</h1>
-      <p>this is some information about me, I guess this will do for now</p>
-    </PageSection>
+      {/* Section Four: Skills */}
+      <PageSection size="large" color="#ddd" background="#f555ff">
+        <h1>This is the about section</h1>
+        <p>this is some information about me, I guess this will do for now</p>
+      </PageSection>
 
-    {/* Section Four: Skills */}
-    <PageSection size="fill" color="#0066dd" background="transparent">
-      <h1>This is the about section</h1>
-      <p>this is some information about me, I guess this will do for now</p>
+      {/* Section Four: Skills */}
+      <PageSection size="fill" color="#0066dd" background="transparent">
+        <h1>This is the about section</h1>
+        <p>this is some information about me, I guess this will do for now</p>
 
-      <div
-        className="pf-subsection"
-        style={{ display: "flex", justifyContent: "center" }}
-      >
-        <PageSection size="large" color="#ddd" background="#0066dd">
-          <h1>This is the about section</h1>
-          <p>this is some information about me, I guess this will do for now</p>
-        </PageSection>
-        <PageSection size="large" color="#ddd" background="#0066dd">
-          <h1>This is the about section</h1>
-          <p>this is some information about me, I guess this will do for now</p>
-        </PageSection>
-      </div>
+        <SectionBlock direction={FlexDirection.Horizontal} background={null}>
+          <PageSection size="large" color="#ddd" background="#0066dd">
+            <h1>This is the about section</h1>
+            <p>
+              this is some information about me, I guess this will do for now
+            </p>
+          </PageSection>
+          <PageSection size="large" color="#ddd" background="#0066dd">
+            <h1>This is the about section</h1>
+            <p>
+              this is some information about me, I guess this will do for now
+            </p>
+          </PageSection>
+        </SectionBlock>
 
-    </PageSection>
+      </PageSection>
 
-    <Link to="/">Go back to the homepage</Link>
+      <Link to="/">Go back to the homepage</Link>
+    </SectionBlock>
   </SectionedView>
 )
 

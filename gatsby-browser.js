6a9aaf60425e53466d8e5932713c46f5a60bf4b8
Josh Liley <=>
2019-11-10 22:30:47 +0000

    carousel componeent and work section basic content

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,6 +9,7 @@ import "typeface-open-sans";
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faChevronCircleLeft, faChevronCircleRight, faCircle as fasCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCircle as farCircle } from '@fortawesome/free-regular-svg-icons'
 
-library.add(fab, faEnvelope);
+library.add(fab, faEnvelope, faChevronCircleLeft, faChevronCircleRight, fasCircle, farCircle);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import {
   SectionType,
 } from "../components/PageSection/sectionBlock";
 import SectionBlock from "../components/PageSection/sectionBlock";
+import Carousel from "../components/PageSection/carousel";
 import PageSection from "../components/PageSection/pageSection";
 import { SectionSize } from "../components/PageSection/pageSection";
 import HoverZoom from "../components/HoverZoom/hoverZoom";
@@ -42,16 +43,32 @@ const NewDesign = () => (
             background={null}
           >
             {/* Section One: About - Image */}
-            <PageSection size={SectionSize.X_Small} color="#ddd" background="#1a2838">
+            <PageSection
+              size={SectionSize.X_Small}
+              color="#ddd"
+              background="#1a2838"
+            >
               <UserImg />
             </PageSection>
             {/* Section One: About - Info */}
-            <PageSection size={SectionSize.Wide} color="#112244" background="#ddd">
+            <PageSection
+              size={SectionSize.Wide}
+              color="#112244"
+              background="#ddd"
+            >
               <h2>Hello, I'm Josh. A front-end web developer.</h2>
               <p style={{ textAlign: "left" }}>
-              I've been working as a developer in Chester for 3 years now. I specialise in creating <span className="pf-standout">React</span> applications with <span className="pf-standout">Typescript</span>. 
-              However, I am familiar with a lot of other frameworks, libraries, and technologies because I work on various projects which require different expertise, allowing me to continually <span className="pf-standout">upskill</span> myself. 
-              I love problem-solving, whether it's diagnosing a technical issue, solving problems for clients, or just finding better ways to work effectively.
+                I've been working as a developer in Chester for 3 years now. I
+                specialise in creating{" "}
+                <span className="pf-standout">React</span> applications with{" "}
+                <span className="pf-standout">Typescript</span>. However, I am
+                familiar with a lot of other frameworks, libraries, and
+                technologies because I work on various projects which require
+                different expertise, allowing me to continually{" "}
+                <span className="pf-standout">upskill</span> myself. I love
+                problem-solving, whether it's diagnosing a technical issue,
+                solving problems for clients, or just finding better ways to
+                work effectively.
               </p>
             </PageSection>
           </SectionBlock>
@@ -73,45 +90,83 @@ const NewDesign = () => (
           background={null}
         >
           {/* Section Two: Projects - P1 */}
-          <PageSection size={SectionSize.Medium} color="#112244" background="#ddd">
+          <PageSection
+            size={SectionSize.Medium}
+            color="#ddd"
+            background="#1a2838"
+          >
             <h3>To do</h3>
-            
-            <SectionBlock direction={FlexDirection.Vertical}
+
+            <SectionBlock
+              direction={FlexDirection.Vertical}
               type={SectionType.Spaced}
-              background={null}>
-                <PageSection size={SectionSize.Fill} color="#1a2838" background="#fff">
-                  <h4>David's Website</h4>
-                </PageSection>
-                <PageSection size={SectionSize.Fill} color="#1a2838" background="#fff">
-                  <h4>Dylans Website</h4>
-                </PageSection>
+              background={null}
+            >
+              <PageSection
+                size={SectionSize.Fill}
+                color="#1a2838"
+                background="#fff"
+              >
+                <h4>David's Website</h4>
+              </PageSection>
+              <PageSection
+                size={SectionSize.Fill}
+                color="#1a2838"
+                background="#fff"
+              >
+                <h4>Dylans Website</h4>
+              </PageSection>
             </SectionBlock>
           </PageSection>
 
           {/* Section Two: Projects - P2 */}
-          <PageSection size={SectionSize.Medium} color="#112244" background="#ddd">
+          <PageSection
+            size={SectionSize.Medium}
+            color="#ddd"
+            background="#1a2838"
+          >
             <h3>In progress</h3>
-            <SectionBlock direction={FlexDirection.Vertical}
+            <SectionBlock
+              direction={FlexDirection.Vertical}
               type={SectionType.Spaced}
-              background={null}>
-                <PageSection size={SectionSize.Fill} color="#1a2838" background="#fff">
-                  <h4>Alexa Skill</h4>
-                </PageSection>
-                <PageSection size={SectionSize.Fill} color="#1a2838" background="#fff">
-                  <h4>Reddit Bot</h4>
-                </PageSection>
+              background={null}
+            >
+              <PageSection
+                size={SectionSize.Fill}
+                color="#1a2838"
+                background="#fff"
+              >
+                <h4>Alexa Skill</h4>
+              </PageSection>
+              <PageSection
+                size={SectionSize.Fill}
+                color="#1a2838"
+                background="#fff"
+              >
+                <h4>Reddit Bot</h4>
+              </PageSection>
             </SectionBlock>
           </PageSection>
 
           {/* Section Two: Projects - P2 */}
-          <PageSection size={SectionSize.Medium} color="#112244" background="#ddd">
+          <PageSection
+            size={SectionSize.Medium}
+            color="#ddd"
+            background="#1a2838"
+          >
             <h3>Completed</h3>
-            <SectionBlock direction={FlexDirection.Vertical}
+            <SectionBlock
+              direction={FlexDirection.Vertical}
               type={SectionType.Spaced}
-              background={null}>
-                <PageSection size={SectionSize.Fill} color="#1a2838" background="#fff">
-                  <h4>Portfolio Site</h4>
-                </PageSection>
+              background={null}
+            >
+              <PageSection
+                size={SectionSize.Fill}
+                color="#1a2838"
+                background="#fff"
+              >
+                <h4>Portfolio Site</h4>
+              </PageSection>
             </SectionBlock>
           </PageSection>
         </SectionBlock>
@@ -125,18 +180,62 @@ const NewDesign = () => (
         navigationPoint={3}
         padding="100px 0"
       >
-        <h2>I currently work at a SharePoint consultancy in Chester</h2>
-        <p>
-          I will put info about my job here i guess, what i do, skills, things
-          ive worked on etc...
-        </p>
+        <h2>My Work History</h2>
+        <Carousel
+          startingSlide={1}
+        >
+          {/* Section Three: Work - Description */}
+          <PageSection
+            size={SectionSize.Fill}
+            color="#112244"
+            background="#ddd"
+          >
+            <h2>I graduated from Sheffield Hallam University</h2>
+            <p>
+              I studied Information technologies with Business from 2012-2016
+            </p>
+
+            <h4>Modules:</h4>
+            <ul>
+              <li>Ive worked on React projects</li>
+              <li>Passed 2 certified microsoft exams</li>
+              <li>Given talks internally and externally on programming</li>
+              <li>Consulted with clients to architect a solution</li>
+              <li>provided first line support to clients</li>
+            </ul>
+          </PageSection>
+          {/* Section Three: Work - Highlights */}
+          <PageSection
+            size={SectionSize.Fill}
+            color="#112244"
+            background="#ddd"
+          >
+            <h2>I currently work at a SharePoint consultancy in Chester</h2>
+            <p>
+              As a developer at WM Reply, I work with a range of clients, that I
+              provide bespoke solutions for their unique problems. This means
+              I'm constantly collaborating on projects with different scopes.
+              This enables me to grow many different skills, in a variety of
+              areas.
+            </p>
+
+            <h4>Highlights:</h4>
+            <ul>
+              <li>Ive worked on React projects</li>
+              <li>Passed 2 certified microsoft exams</li>
+              <li>Given talks internally and externally on programming</li>
+              <li>Consulted with clients to architect a solution</li>
+              <li>provided first line support to clients</li>
+            </ul>
+          </PageSection>
+        </Carousel>
       </PageSection>
 
       {/* Section Four: Skills */}
       <PageSection
         size={SectionSize.Fill}
         color="#ddd"
-        background="#f555ff"
+        background="#112244"
         navigationPoint={4}
         padding="80px 0"
       >
@@ -163,13 +262,21 @@ const NewDesign = () => (
           background={null}
         >
           {/* Section Four: Mu Feed */}
-          <PageSection size={SectionSize.Wide} color="#ddd" background="#0066dd">
+          <PageSection
+            size={SectionSize.Wide}
+            color="#ddd"
+            background="#0066dd"
+          >
             <h2>Stuff im up to</h2>
             <p>Blah blah blah...</p>
           </PageSection>
 
           {/* Section Four: Mu Feed */}
-          <PageSection size={SectionSize.Wide} color="#ddd" background="#0066dd">
+          <PageSection
+            size={SectionSize.Wide}
+            color="#ddd"
+            background="#0066dd"
+          >
             <h2>What i've learnt this week</h2>
             <p>What ive learnt this week will shock you...</p>
           </PageSection>
